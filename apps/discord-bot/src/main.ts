7f05ff48fe5ca808a211ff21d9c1ccd54893f3e3
gideonoludeyi <gooludeyi@gmail.com>
2023-02-06 13:37:22 -0500

    feat(discord-bot): discord app boilerplate
    setup minimal discord bot application

--- apps/discord-bot/src/main.ts
+++ apps/discord-bot/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const { CLIENT_TOKEN } = process.env;
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(CLIENT_TOKEN);
